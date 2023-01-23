70e0c52715f7fbf29d5bb477b7e3b9813930e808
Muhammed Solmaz <muhammed.slmz@gmail.com>
2023-01-23 14:18:48 +0300

    Min & max value calculator in array.

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,17 @@
 public class Main {
     public static void main(String[]args){
-        int[]list={1,45,6,34,8,76};
-        double sum=0.0;
-        for(int i=0;i< list.length;i++){
-            sum+=list[i];
-        }
-        double average=sum/list.length;
-        System.out.println(average);
+       int []list={1,54,9,-76,-9,44,-7};
+       int max=list[0];
+       int min=list[0];
+       for(int i: list){
+           if(i>max){
+               max=i;
+           }
+           if(i<min){
+               min=i;
+           }
+       }
+        System.out.println("Max: "+max);
+        System.out.println("Min: "+min);
     }
 }
