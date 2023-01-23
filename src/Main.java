26518847ee69ffa01255e244155c558279d811eb
Muhammed Solmaz <muhammed.slmz@gmail.com>
2023-01-23 14:51:32 +0300

    Printed letter 'P'

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,15 @@
-public class Main {
+public class Main{
     public static void main(String[]args){
-       int []list={1,54,9,-76,-9,44,-7};
-       int max=list[0];
-       int min=list[0];
-       for(int i: list){
-           if(i>max){
-               max=i;
-           }
-           if(i<min){
-               min=i;
-           }
-       }
-        System.out.println("Max: "+max);
-        System.out.println("Min: "+min);
+        int [][]letter=new int[6][4];
+        for (int i=0;i< letter.length;i++){
+            for (int j=0;j<letter[i].length;j++){
+                if(i==0||i==3||j==0)
+                    System.out.print(" * ");
+                else if (j==3&&i!=4&&i!=5) {
+                    System.out.print(" * ");
+                } else System.out.print("   ");
+            }
+            System.out.println();
+        }
     }
 }
