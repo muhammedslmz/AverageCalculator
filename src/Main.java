e7a2cb7c6deb4829e1fd6ad70238bc3e90da5b8d
Muhammed Solmaz <muhammed.slmz@gmail.com>
2023-01-23 16:46:30 +0300

    All object are taken by users.

--- src/Main.java
+++ src/Main.java
@@ -1,32 +1,18 @@
 import java.util.Arrays;
+import java.util.Scanner;
+
 
 public class Main{
-    static boolean isFind(int[]arr ,int value){
-        for(int i: arr){
-            if(i==value){
-                return true;
-            }
-        }
-        return false;
-    }
     public static void main(String[]args){
-        int []list={2,4,456,3,9,88,88,3,3,75,56,2};
-        int[]duplicate=new int[list.length];
-        int startIndex  =0;
-        for(int i=0;i< list.length;i++) {
-            for (int j = 0; j < list.length; j++){
-                if(i!=j  &&  (list[i]==list[j])){
-                    if(!isFind(duplicate,list[i])){
-                        duplicate[startIndex++]=list[i];
-                    }
-                    break;
-                }
-            }
+        Scanner input=new Scanner(System.in);
+        System.out.print("Enter the size of array: ");
+        int size=input.nextInt();
+        int[] list=new int[size];
+        for (int i=0;i< list.length;i++){
+            System.out.print("Enter the "+(i+1)+". element of array: ");
+            list[i]=input.nextInt();
         }
-        for(int value:duplicate){
-            if(value!=0){
-                System.out.print(value+",");
-            }
-        }
+        Arrays.sort(list);
+        System.out.println(Arrays.toString(list));
     }
 }
