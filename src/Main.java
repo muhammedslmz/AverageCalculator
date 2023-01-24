84cb8614f570bc737de2d60e477f7a6272e36c10
Muhammed Solmaz <muhammed.slmz@gmail.com>
2023-01-24 19:36:59 +0300

    Matris & Transpoze

--- src/Main.java
+++ src/Main.java
@@ -1,33 +1,42 @@
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class Main{
     public static void main(String[]args){
-        int []list={10,20,20,10,10,20,5,20};
-        int []cover=new int[list.length];
-        System.out.println("Array: "+ Arrays.toString(list));
-        int sum=0;
-        int n=0;
-        System.out.println("Repeating numbers");
-        for(int i=0;i< list.length;i++){
-            for (int k=0;k< list.length;k++){
+        Scanner inp =new Scanner(System.in);
+        System.out.print("Enter the number of row: ");
+        int row=inp.nextInt();
 
-                if(list[i]==list[k]){
-                    sum++;
-                }
+        System.out.print("Enter the number of colon: ");
+        int col=inp.nextInt();
+        int [][]list=new int[row][col];
+        int[][]listTranspoze=new int[col][row];
+        for (int i=0;i< list.length;i++){
+            for (int j=0;j<list[0].length;j++){
+                list[i][j]=inp.nextInt();
             }
-            for(int j=0;j< list.length;j++){
-                if((list[i]==cover[j])){
-                    n++;
-                }
-            }
-            if(n==0){
-                System.out.println("The number "+list[i]+" was repeated "+sum+" times.");
-            }
-            n=0;
-            sum=0;
-            cover[i]=list[i];
-
         }
+        System.out.println("Matris :");
+        for (int[]a:list){
+            for (int b:a){
+                System.out.print(b+" \t");
+            }
+            System.out.println();
+        }
+        System.out.println("Transpoze :");
+        for(int i=0;i< list.length;i++){
+            for (int j =0; j<list[0].length;j++){
+                listTranspoze[j][i]=list[i][j]  ;
+            }
+        }
+
+        for (int[] a: listTranspoze){
+            for (int b: a){
+                System.out.print(b+" \t");
+            }
+            System.out.println();
+        }
+
 
     }
 }
